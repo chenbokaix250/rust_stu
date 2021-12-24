95445dab1d67495b8c62c665a98475322e5f96a9
chenbokaix250 <chenbokaix250@outlook.com>
2021-12-25 01:02:37 +0800

    main

--- variabels/src/main.rs
+++ variabels/src/main.rs
@@ -1,6 +1,14 @@
 const MAX_POINTS:u32 = 100_000;
 fn main() {
     println!("Hello, world!");
-    println!("value is {}",MAX_POINTS)
 
+    let mut x =5;
+    println!("The calue of x is {}",x);
+
+    x = 6;
+    println!("The calue of x is {}",x);
+    println!("value is {}",MAX_POINTS);
+
+    let guess:u32 = "42".parse().expect("Not a nunber");
+    println!("{}",guess);
 }
